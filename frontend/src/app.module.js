6825ba65d1f4cba28c947273025ecdee8f2df9c0
mlafroce <matias@shishi>
2019-02-02 14:18:56 -0300

    TCK #3 Hola mundo en angular y webpack

---
+++ frontend/src/app.module.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import angular from 'angular';
+import './components/assets/app.component.css';
+
+import AppModule from './components/app.module';
+import ApartmentController from './components/controllers/apartmentController';
