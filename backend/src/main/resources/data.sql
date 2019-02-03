8dbce2eea9676e346813c5f136c1da3968f8ac6b
mlafroce <mlafroce@gmail.com>
2019-02-03 20:34:53 -0300

    Close #5 agrega datos iniciales

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO apartment (id, location, rooms, area, rent_per_day, rent_per_month) VALUES
+  (1, 'Aldo Bonzi', 1, 40, 100, 4000),
+  (2, 'Burzaco', 3, 85, 200, 7500);
---
+++ backend/src/test/resources/data.sql
@@ -0,0 +1,2 @@
+-- cleanup
+delete from apartment;
