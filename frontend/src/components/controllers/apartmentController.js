d18de69a51abbdf1610a6c725b9a2d34af152c86
mlafroce <mlafroce@gmail.com>
2019-02-06 09:17:54 -0300

    TCK #8 acciones de presupuestar y alquilar

--- frontend/src/components/controllers/apartmentController.js
+++ frontend/src/components/controllers/apartmentController.js
@@ -12,13 +12,56 @@ angular.module('appModule')
     ['$scope', 'apartmentService',
     function($scope, apartmentService) {
       // Carga los departamentos iniciales
-      apartmentService.list()
-        .then(function(response){
-            $scope.apartments = response.data;
-        }, function(error, status){
-            console.log("Error: ", error, status);
-        }); //fin then()
-      console.log('apartments:', $scope.apartments);
+      $scope.init = function() {
+        apartmentService.list()
+          .then(function(response){
+              $scope.apartments = response.data;
+          }, function(error, status){
+              console.log("Error: ", error, status);
+          }); //fin then()
+      }
+
+      /**
+      * Realiza una estimación con los datos del departamento activo
+      * y la muestra en el popup de alquiler
+      */
+      $scope.estimate = function() {
+        var params = {
+          "username": $scope.username,
+          "days": $scope.days,
+          "months": $scope.months
+        }
+        apartmentService.estimate($scope.apartmentId, params)
+          .then(function(response){
+              $scope.estimation = response.data;
+          }, function(error, status){
+              console.log("Error: ", error, status);
+          }); //fin then()
+      }
+
+      /**
+      * Marca un departamento como activo antes de abrir el popup de alquiler
+      */
+      $scope.selectApartment = function(id) {
+        $scope.rentSuccessful = false;
+        $scope.apartmentId = id;
+      }
+
+      /**
+      * Realiza un alquiler (guarda en la base de datos el nombre de usuario)
+      */
+      $scope.rent = function() {
+        var data = {
+          "username": $scope.username
+        }
+        apartmentService.rent($scope.apartmentId, data)
+          .then(function(response){
+              $scope.rentSuccessful = true
+          }, function(error, status){
+              console.log("Error: ", error, status);
+          }); //fin then()
+      }
+      $scope.init();
     }
 ]);
 
--- frontend/src/components/services/apartmentService.js
+++ frontend/src/components/services/apartmentService.js
@@ -1,7 +1,24 @@
 angular.module('appModule')
   .service('apartmentService', ['$http', function apartmentService($http) {
+    /**
+    * Trae una lista de todos los edificios disponibles
+    */
     this.list = function(){
         return $http.get("/apartments/");
+    }
+    /**
+    * Realiza un presupuesto del alquiler
+    */
+    this.estimate = function(apartmentId, params){
+        // No me sale la sintaxis /ruta/:param/ruta
+        return $http.get("/apartments/" + apartmentId + "/estimate", {params: params});
+    }
+    /**
+    * Realiza un alquiler
+    */
+    this.rent = function(apartmentId, data){
+        // No me sale la sintaxis /ruta/:param/ruta
+        return $http.post("/apartments/" + apartmentId + "/rent", data);
     }
   }
 ]);
